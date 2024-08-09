e4b6590d6df3885548a040db178eed577ee4a94e
Ramz Gulliev <107952582+ramseyuz@users.noreply.github.com>
2024-08-09 11:17:30 +0500

    Update FridgeAlgorithm.java

--- src/main/java/com/epam/mjc/stage0/FridgeAlgorithm.java
+++ src/main/java/com/epam/mjc/stage0/FridgeAlgorithm.java
@@ -11,6 +11,13 @@ public class FridgeAlgorithm {
      * P.S All methods of interaction with the fridge can be found inside the {@see Fridge} class.
      */
     public void fridgeAlgorithm(Fridge fridge) {
+        
+        for(int i=0; i<fridge.length;i++){
+        if (fridge[i]="milk"){
+            return fridge[i];
+        }
+        
+        }
 
     }
 }
